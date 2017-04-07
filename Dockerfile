ecb74b89027b897122544729b56a9996bdd98ae8
sgalkina <galkina.s.a@gmail.com>
2017-04-07 14:03:18 +0200

    feat: reflect service

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.5
+FROM python:3.6
 
 ADD requirements.txt requirements.txt
 RUN pip install -r requirements.txt
--- id_mapper/app.py
+++ id_mapper/app.py
@@ -20,6 +20,7 @@ from aiohttp import web
 from venom.rpc import Service, Venom
 from venom.rpc.comms.aiohttp import create_app
 from venom.rpc.method import http
+from venom.rpc.reflect.service import ReflectService
 from venom.exceptions import NotFound
 
 from id_mapper.stubs import QueryRequest, QueryResponse
@@ -29,7 +30,10 @@ from id_mapper.graph import find_match, NoSuchNode
 class IDMapping(Service):
     @http.GET('./query')
     def query(self, request: QueryRequest) -> QueryResponse:
-        graph = Graph(host=os.environ['DB_PORT_7687_TCP_ADDR'], password=os.environ['NEO4J_PASSWORD'])
+        graph = Graph(
+            host=os.environ['DB_PORT_7687_TCP_ADDR'],
+            password=os.environ['NEO4J_PASSWORD']
+        )
         try:
             return QueryResponse(ids=find_match(
                 graph, request.id, request.db_from, request.db_to
@@ -39,6 +43,8 @@ class IDMapping(Service):
 
 venom = Venom()
 venom.add(IDMapping)
+venom.add(ReflectService)
+
 
 app = create_app(venom)
 
