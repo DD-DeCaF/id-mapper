6ba75311f6a570a4ff620cbe4cef71247ed378b4
Ali Kaafarani <ali@kvikshaug.no>
2019-10-03 10:27:23 +0200

    refactor: update deprecated import

--- src/id_mapper/app.py
+++ src/id_mapper/app.py
@@ -21,7 +21,7 @@ import logging.config
 from flask import Flask
 from flask_cors import CORS
 from raven.contrib.flask import Sentry
-from werkzeug.contrib.fixers import ProxyFix
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 from . import errorhandlers
 
